9b9902a9338c14fb91675fd9b9cc6bdd6fd6612d
Alex Jeffrey <alex@alexjeffrey.me>
2014-05-07 22:12:41 +0100

    Fixed phpunit/composer collision

--- tests/ConnectionTest.php
+++ tests/ConnectionTest.php
@@ -1,5 +1,4 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
 
 class ConnectionTest extends PHPUnit_Framework_TestCase {
 	/**
--- tests/QueryTest.php
+++ tests/QueryTest.php
@@ -1,5 +1,4 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
 
 class QueryTest extends PHPUnit_Framework_TestCase {
 
